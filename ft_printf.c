bd42030d637d889c85da389519d8db941cb152b8
dbessa <danielbessa01@gmail.com>
2023-10-18 17:10:14 -0300

    prototyping with man va_arg example

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,36 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 10:20:59 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/18 10:26:30 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/18 17:09:28 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_printf(const char *fmt, ...)
 {
-	
+	va_list	ap;
+	int	d;
+	char	c;
+	char		*s;
+
+	va_start(ap, fmt);
+	while (*fmt)
+		switch (*fmt++) {
+		case 's':              /* string */
+			s = va_arg(ap, char *);
+			printf("string %s\n", s);
+		break;
+		case 'd':              /* int */
+			d = va_arg(ap, int);
+			printf("int %d\n", d);
+			break;
+		case 'c':              /* char */
+                   /* need a cast here since va_arg only
+                      takes fully promoted types */
+			c = (char) va_arg(ap, int);
+			printf("char %c\n", c);
+			break;
+		}
+	va_end(ap);
 }
