e44d587da03e99fe8f8cd2f8e9c657133e083c5f
dbessa <danielbessa01@gmail.com>
2023-10-20 09:20:39 -0300

    adding lower and upper case letter cases

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -6,28 +6,32 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 15:25:12 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/19 18:51:25 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/20 08:31:25 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	to_char(unsigned int n)
+static void	to_char(unsigned int n, int arg)
 {
-	const char	*alphaHex = "0123456789ABCDEF";
+	const char	*alphaUpp = "0123456789ABCDEF";
+	const char	*alphaLow = "0123456789abcdef";
 
-	write(1, &alphaHex[n], 1);
+	if (arg == 'X')
+		write(1, &alphaUpp[n], 1);
+	else if (arg == 'x')
+		write(1, &alphaLow[n], 1);
 }
 
-void	ft_puthexa(unsigned int d)
+void	ft_puthexa(unsigned int d, int arg)
 {
 	int	r;
 
 	r = d % 16;
 	if (d - r == 0)
-		to_char(r);
+		to_char(r, arg);
 	else
-		ft_puthexa((d - r) / 16);
+		ft_puthexa((d - r) / 16, arg);
 	if (d - r != 0)
-		to_char(r);
+		to_char(r, arg);
 }
