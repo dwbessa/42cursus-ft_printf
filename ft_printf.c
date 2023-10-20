3c02c6103a780ea89d9012f72d747d787e2bac81
dbessa <danielbessa01@gmail.com>
2023-10-20 14:26:53 -0300

    commenting main

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 10:20:59 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/20 13:51:49 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/20 14:26:06 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,15 @@ int	ft_printf(const char *format, ...)
 	return (ret);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	int	x = 29;
-	int	*ptr = &x;
-	char str[] = "THE SKY IS BLUE";
-	ft_printf("Eu sou o Daniel, mas pode me chamar de %c. %s, tenho %d anos e %umg de peso.\nMinha idade em hexadecimal é %x ou %X.\nTenho %i%% de bateria e o endereço de memória da minha idade é %p.\nObrigado por ler até aqui.\n\n", 'D', "Bessa", x, 4294967295, x, x, 97, ptr);
-
-	printf("\nThe value returned by printf() for the above string is : %d\n\n", printf("%s", str));
-	ft_printf("\nThe value returned by ft_printf() for the above string is : %d", ft_printf("%s", str));
-}
+//#include <stdio.h>
+//int	main(void)
+//{
+//	int	x = 29;
+//	int	*ptr = &x;
+//	char str[] = "THE SKY IS BLUE";
+//	ft_printf("Eu sou o Daniel, mas pode me chamar de %c. %s e tenho %d anos e %umg de peso. Minha idade em hexadecimal é %x ou %X. Tenho %i%% de bateria e o endereço de memória da minha idade é %p. Obrigado por ler até aqui.\n", 'D', "Bessa", x, 4294967295, x, x, 97, ptr);
+//
+//	printf("\nThe value returned by printf() for the above string is : %d\n\n", printf("%s", str));
+//	ft_printf("\nThe value returned by ft_printf() for the above string is : %d", ft_printf("%s", str));
+//}
+//
