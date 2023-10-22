943da6a718c1786f5780a49f15d3382086e1fe18
dwbessa <danielbessa01@gmail.com>
2023-10-22 19:58:49 -0300

    removing main to adjust with norminette

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 10:20:59 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/22 19:35:47 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/22 19:58:18 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,3 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 	return (ret);
 }
-
-//#include <stdio.h>
-//int	main(void)
-//{
-//	int	x = 29;
-//	int	*ptr = &x;
-//	char str[] = "THE SKY IS BLUE";
-//	ft_printf("Eu sou o Daniel, mas pode me chamar de %c. %s e tenho %d anos e %umg de peso. Minha idade em hexadecimal é %x ou %X. Tenho %i%% de bateria e o endereço de memória da minha idade é %p. Obrigado por ler até aqui.\n", 'D', "Bessa", x, 4294967295, x, x, 97, ptr);
-//
-//	printf("\nThe value returned by printf() for the above string is : %d\n\n", printf("%s", str));
-//	ft_printf("\nThe value returned by ft_printf() for the above string is : %d", ft_printf("%s", str));
-//}
-//
