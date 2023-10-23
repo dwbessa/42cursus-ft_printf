f1e3c6cc36a90f1928689919d6b781db21ff378e
dwbessa <danielbessa01@gmail.com>
2023-10-22 21:04:59 -0300

    applying changes for consistency

--- srcs/ft_putunsnbr.c
+++ srcs/ft_putunsnbr.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 09:18:12 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/22 19:40:26 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/22 21:03:21 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,9 @@ int	ft_putunsnbr(unsigned int nb)
 	unsigned int	i;
 
 	i = 0;
-	if (nb >= 1 && nb <= 9)
+	if (nb < 10)
 	{
 		ft_putchar(nb + '0');
-		i++;
-	}
-	else if (nb == 0)
-	{
-		ft_putchar('0');
 		i++;
 	}
 	else
